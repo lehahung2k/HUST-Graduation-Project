08dc5db91f123518a9c4cba6df18645ceadfb59b
Le Ha Hung <hung_lh@flinters.vn>
2023-02-21 15:39:05 +0700

    front-end: using SB Admin2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './assets/logo.svg';
 import './App.css';
+import './styles/sb-admin-2.min.css'
+import {BrowserRouter as Router} from "react-router-dom";
+import {Admin} from "./pages/Admin/Admin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="wrapper">
+      <Router>
+          <Admin/>
+      </Router>
     </div>
   );
 }
